6010220b45f4ad006e254aea61fd7f68cd34026d
Darby Smith <newman.darby2@gmail.com>
2024-02-12 11:16:12 -0700

    setup app server

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+// required modules are imported and may have to install types for some
+import express from "express" // framework
+import * as dotenv from "dotenv" // handle env variables
+import cors from "cors" // enable Cross-Origin Resource sharing
+import helmet from "helmet" // add security to HTTP responses
+
+dotenv.config()
+
+// checks if there is a PORT env variable, if not log to console
+if (!process.env.PORT) {
+    console.log(`No port value specified...`)
+}
+
+// convert the string value for port to int
+const PORT = parseInt(process.env.PORT as string, 10)
+
+// create instance of express 
+const app = express()
+
+// middleware functions added to express
+app.use(express.json())     // parse JSON for incoming requests
+app.use(express.urlencoded({extended : true}))      // parse URL-encoded bodies of incoming requests
+app.use(cors())     // enable Cross-Origin Resource sharing
+app.use(helmet())   // enhance security by setting some HTTP headers
+
+// app is listening on the specified port in env 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
